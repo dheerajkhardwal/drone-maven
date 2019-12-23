e4006c106fffdb342f90574fc969903d9b275bb9
Dheeraj Khardwal <dheeraj@khardwal.com>
2019-12-23 15:15:48 +0000

    chore: init skeleton

---
+++ cmd/main.go
@@ -0,0 +1,58 @@
+// Copyright (c) 2019, the Drone Plugins project authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by an Apache 2.0 license that can be
+// found in the LICENSE file.
+
+// DO NOT MODIFY THIS FILE DIRECTLY
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/drone-plugins/drone-plugin-lib/pkg/urfave"
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+
+	"github.com/dheerajkhardwal/drone-maven/pkg/plugin"
+)
+
+var (
+	version = "unknown"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = ""
+	app.Usage = ""
+	app.Action = run
+	app.Flags = append(settingsFlags(), urfave.Flags()...)
+
+	// Run the application
+	if err := app.Run(os.Args); err != nil {
+		logrus.Fatal(err)
+	}
+}
+
+func run(ctx *cli.Context) error {
+	urfave.LoggingFromContext(ctx)
+
+	plugin := plugin.New(
+		settingsFromContext(ctx),
+		urfave.PipelineFromContext(ctx),
+		urfave.NetworkFromContext(ctx),
+	)
+
+	// Validate the settings
+	if err := plugin.Validate(); err != nil {
+		return fmt.Errorf("Validation failed %w", err)
+	}
+
+	// Run the plugin
+	if err := plugin.Exec(); err != nil {
+		return fmt.Errorf("Execution failed %w", err)
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/dheerajkhardwal/drone-maven
+
+go 1.13
+
+require (
+	github.com/drone-plugins/drone-plugin-lib v0.1.1
+	github.com/sirupsen/logrus v1.4.2
+	github.com/urfave/cli/v2 v2.0.0
+)
